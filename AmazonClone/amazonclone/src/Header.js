5bf1e01361429bbf5c5f5e28a9bdebd90260b0a5
Ofego-Goanna <ofego@goanna.group>
2021-07-20 21:59:31 +1000

    update

---
+++ AmazonClone/amazonclone/src/Header.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import "./Header.css";
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+function Header(){
+    return (
+        <div className="header">
+            <img
+            className="header__logo"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeKYiC-kZrADgVQ4uuSNPXlKsG5crJSLjUBg&usqp=CAU"
+            />
+
+            <div className="header__search">
+                <input className="header__searchInput"
+                type="text"/>
+                <SearchIcon className="header__searchIcon"/>
+            </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionLine1">
+                        Hello Guests
+                    </span>
+                    <span className="header__optionLine2">
+                        Sign In
+                    </span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLine1">
+                        Returns
+                    </span>
+                    <span className="header__optionLine2">
+                        & Orders
+                    </span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionLine1">
+                        Your
+                    </span>
+                    <span className="header__optionLine2">
+                        Prime
+                    </span>
+                </div>
+                
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon/>
+                    <span className="header__optionLine2
+                    header__basketCount">
+                        0
+                    </span>
+                </div>
+            </div>
+
+        </div>
+      );
+    }
+    
+    export default Header;
+    
